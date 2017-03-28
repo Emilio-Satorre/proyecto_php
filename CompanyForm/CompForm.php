d460f2f8c282d66dbcf5a96d9df2d1e6e0b6aae2
Emilio-Satorre <satorre83@gmail.com>
2017-03-28 11:59:05 +0200

    Formulario registro empresa con CSS

---
+++ CompanyForm/CompForm.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Registro Empresa</title>
+    <link rel="stylesheet" type="text/css" href="CompForm.css">
+</head>
+<body>
+    <h2>Registro Empresa</h2>
+    <form action="POST">
+        <label>Empresa</label>
+        <input type="text" name="user" placeholder="nombre de la empresa" required><br><br>
+        <label>Correo electrónico</label>
+        <input type="email" name="mail" placeholder="correo electrónico" required><br><br>
+        <label>Password</label>
+        <input type="password" name="pass" placeholder="introduzca contraseña" requires><br><br>
+        <input type="submit" name="" value="Enviar" id="button">
+    </form>
+</body>
+</html>
