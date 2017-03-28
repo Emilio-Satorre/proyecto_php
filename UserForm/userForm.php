bfcc8b6021f44f61efa18fb602385cc051a9ddd8
Emilio-Satorre <satorre83@gmail.com>
2017-03-28 13:52:19 +0200

    Formulario de usuario con comparacion de email

--- UserForm/userForm.php
+++ UserForm/userForm.php
@@ -4,24 +4,47 @@
 	<meta charset="UTF-8">
 	<title>Registro</title>
 	<link rel="stylesheet" type="text/css" href="formStyle.css">
+
 </head>
 <body>
+	<?php
+		require_once "conexion.php";
 
-	<h2>Registro:</h2>
+		if(! empty($_POST)){
+			if(mailExists()){
+				echo "Este correo ya esta en uso";
+			}else{
+		 		register_players($_POST['username'],$_POST['email'],$_POST['password']);
+			}
+		}
 
-	<form action="POST">
 
+		function register_players($username,$email,$password){
+		 	global $pdo;
+		 	$stmt = $pdo->prepare('INSERT INTO players (user,email,password) VALUES (:user,:mail,:pass)');
+		 	$stmt->execute([':user'=>$username,':mail'=>$email,':pass'=>$password]);
+		}
+		function mailExists(){
+			global $pdo;
+			$stmt=$pdo->prepare('SELECT * FROM players');
+			$stmt->execute();
+			$user=$stmt->fetch();
+			if($user['email'] === $_POST['email']){
+				return true;
+			}
+			return false;
+		}
+	?>
+
+	<h2>Registro:</h2><br>
+	<form method="POST" action="">
 		<label>Usuario:</label>
 		<input type="text" name="username" placeholder="Escriba aquí su nombre de usuario" required><br><br>
-
 		<label>Correo electrónico:</label>
 		<input type="email" name="email" placeholder="Escriba aquí su correo electrónico" required><br><br>
-
 		<label>Contraseña:</label>
 		<input type="password" name="password" placeholder="Escriba aquí su contraseña" required><br><br>
-
-		<input type="submit" name="sent" value="Enviar" id="button">
-
+		<input type="submit" name="" value="Enviar" id="button">
 	</form>
 </body>
 </html>
