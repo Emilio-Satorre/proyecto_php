745ff44cd5fe5f2d6d06e7588150c7479947fb59
Emilio-Satorre <satorre83@gmail.com>
2017-03-28 12:06:56 +0200

    formulario simple usuario con CSS

--- UserForm/userForm.php
+++ UserForm/userForm.php
@@ -12,13 +12,7 @@
 	<form action="POST">
 
 		<label>Usuario:</label>
-		<input type="text" name="user" placeholder="Escriba aquí su nombre de usuario" required><br><br>
-
-		<label>Nombre:</label>
-		<input type="text" name="username" placeholder="Escriba aquí su nombre" required><br><br>
-
-		<label>Apellidos:</label>
-		<input type="text" name="lastname" placeholder="Escriba aquí sus apellidos" required><br><br>
+		<input type="text" name="username" placeholder="Escriba aquí su nombre de usuario" required><br><br>
 
 		<label>Correo electrónico:</label>
 		<input type="email" name="email" placeholder="Escriba aquí su correo electrónico" required><br><br>
@@ -27,7 +21,7 @@
 		<input type="password" name="password" placeholder="Escriba aquí su contraseña" required><br><br>
 
 		<input type="submit" name="sent" value="Enviar" id="button">
-		
+
 	</form>
 </body>
 </html>
